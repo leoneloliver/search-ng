2e56c0b2f9d1852eb5a20c7be0ddaa8b088115fd
Leonel Oliveira - OnR <loliveira@hq.onresearch.com>
2019-01-08 16:50:15 -0500

    implementing list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
-  
+  // dataset = ['MDB', 'Angular', 'Bootstrap', 'Framework', 'SPA', 'React', 'Vue'];
+  // searchText = "";
 }
